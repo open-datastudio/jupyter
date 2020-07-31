a8807546d9856dc4848c0d5084f3cd9a0319a518
Lee moon soo <leemoonsoo@gmail.com>
2020-07-31 16:57:55 -0700

    Build gpu enabled docker based image (#8)
    * build image based on nvidia docker
    
    * run through sh
    
    * run from /var/run
    
    * print
    
    * try tmp dir
    
    * update
    
    * update
    
    * print build context
    
    * update
    
    * push image
    
    * update

---
+++ build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Custom builder script for Skaffold
+# https://skaffold.dev/docs/pipeline-stages/builders/custom/
+#
+
+# Generate jupyter docker image using nvidia docker for gpu support.
+# Otherwise, Dockerfile_staroid would be enough.
+git clone https://github.com/iot-salzburg/gpu-jupyter.git
+cd gpu-jupyter
+
+# generate Dockerfile
+./generate-Dockerfile.sh -s --no-datascience-notebook --no-useful-packages
+cd .build
+
+# patch Dockerfile. Want to keep minimal jupyter on gpu
+cat Dockerfile | awk '!p;/Dependency: jupyter\/scipy-notebook/{p=1}' > Dockerfile_minimal
+
+# append staroid Dockerfile
+cat ../../Dockerfile_staroid | grep -v ^FROM >> Dockerfile_minimal
+cp ../../requirements.txt ./
+
+# print Dockerfile
+cat Dockerfile_minimal
+
+# build
+docker build -f Dockerfile_minimal -t $IMAGE .
+
+if $PUSH_IMAGE; then
+  docker push $IMAGE
+fi
