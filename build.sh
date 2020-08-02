ef02e84c992b74dab2f158c17b6c40dbbb1f35de
Lee moon soo <leemoonsoo@gmail.com>
2020-08-01 18:18:29 -0700

    update

--- build.sh
+++ build.sh
@@ -3,13 +3,17 @@
 # https://skaffold.dev/docs/pipeline-stages/builders/custom/
 #
 
+echo "image -- $IMAGE;"
+DOCKER_STACK_COMMIT=1a66dd36ff821bcef814afe86dbc3dba8cd2198d
+GPU_JUPYTER_COMMIT=3c350f0c933156edb49ebccb23142b890c232df6
+
 # Generate jupyter docker image using nvidia docker for gpu support.
 # Otherwise, Dockerfile_staroid would be enough.
 git clone https://github.com/iot-salzburg/gpu-jupyter.git
 cd gpu-jupyter
+git checkout $GPU_JUPYTER_COMMIT
 
 # generate Dockerfile
-DOCKER_STACK_COMMIT=1a66dd36ff821bcef814afe86dbc3dba8cd2198d
 ./generate-Dockerfile.sh -c ${DOCKER_STACK_COMMIT} -s --no-datascience-notebook --no-useful-packages
 cd .build
 
